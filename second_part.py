845ac94e0b06d38acf8c4f02aeaf0846340bc1a8
Florian Dima <dflorian251@gmail.com>
2024-04-30 18:30:35 +0300

    Basic functionality for plotting

--- second_part.py
+++ second_part.py
@@ -1,16 +1,32 @@
 # The second part of the semester's project
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 
-N = 140
+N = 1000
 
+# Network topologies
 regular = nx.random_regular_graph(4, N)
 random = nx.gnp_random_graph(N, 0.25) # Returns a Gn,p random graph, also known as an Erdős-Rényi graph or a binomial graph
 rand_geometric = nx.random_geometric_graph(N, 120, 100)
 small_world = nx.watts_strogatz_graph(N, 4, 0.2)
-scale_free = nx.barabasi_albert_graph(N, 5) # Generate a scale-free graph using the Barabási-Albert model
+scale_free = nx.barabasi_albert_graph(N, 2) # Generate a scale-free graph using the Barabási-Albert model
 
-nx.draw(scale_free, with_labels=True, node_color='skyblue', font_size=12)
+node_degrees = nx.degree(scale_free)
 
-# Display the graph
+
+
+# Calculate the desired metrics
+# Y = degree, X = node
+
+y = []
+x = []
+for node, degree in node_degrees:
+    x.append(node)
+    y.append(degree)
+    print(f"Node {node} has degree {degree}")
+
+
+plt.plot(x, y)
+
 plt.show()
